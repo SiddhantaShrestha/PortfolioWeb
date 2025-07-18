dc1cf9678ea48ead3ff3f3c39732be8d56ca8a5f
Siddhanta Shrestha <siddhanta.shrestha2004@gmail.com>
2025-07-18 09:34:43 +0545

    works and contact, designs

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+const navs = document.querySelectorAll(".nav-list li");
+const cube = document.querySelector(".box");
+
 const resumeLists = document.querySelectorAll(".resume-list");
 const resumeBoxes = document.querySelectorAll(".resume-box");
 
 const portfolioLists = document.querySelectorAll(".portfolio-list");
 const portfolioBoxes = document.querySelectorAll(".portfolio-box");
+
+//navbar sections
+navs.forEach((nav, idx) => {
+  nav.addEventListener("click", () => {
+    document.querySelector(".nav-list li.active").classList.remove("active");
+    nav.classList.add("active");
+
+    cube.style.transform = `rotateY(${idx * -90}deg)`;
+  });
+});
 
 //resume tab-list
 resumeLists.forEach((list, idx) => {
